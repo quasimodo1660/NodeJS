19c1557da738d451ffbd5dd607325741c3b59547
Quasimodo <quasimodo1660@gmail.com>
2018-10-16 16:05:21 -0700

    2

--- index.js
+++ index.js
@@ -1,30 +1,117 @@
-//Dependencies
-const http = require('http');
-const url=require('url');
+// Dependencies
+var http = require('http');
+var https=require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config')
+var fs = require('fs')
+
+// Instantiate the HTTP server
+var httpServer = http.createServer(function(req,res){
+    unifiedServer(req,res) 
+});
+
+// Start the server
+httpServer.listen(config.httpPort,function(){
+  console.log('The server is up and running on port '+config.httpPort+' in '+config.envName+' mode');
+});
+
+// Instantiate the HTTPS server
+var httpsServerOption={
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+}
+var httpsServer = https.createServer(httpsServerOption,function(req,res){
+    unifiedServer(req,res) 
+});
+
+// Start the server
+httpsServer.listen(config.httpsPort,function(){
+  console.log('The server is up and running on port '+config.httpsPort+' in '+config.envName+' mode');
+});
 
 
 
 
 
-const server=http.createServer((req,res)=>{
-    // console.log(req)
-    var parsedUrl=url.parse(req.url,true)
-    // console.log(parsedUrl)
+// Server Logic
+var unifiedServer = (req,res)=>{
+    // Parse the url
+    var parsedUrl = url.parse(req.url, true);
 
-    var path=parsedUrl.pathname
-    var trimmedPath= path.replace(/^\/+|\/+$/g,'')
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    console.log(parsedUrl.query)
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
 
-    console.log(req.method.toLowerCase())
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
 
-    console.log(trimmedPath)
-    
-    res.end('You are SB\n')
+    //Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload,if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+    buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+    buffer += decoder.end();
+
+    // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    var data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data,function(statusCode,payload){
+
+    // Use the status code returned from the handler, or set the default status code to 200
+    statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+    // Use the payload returned from the handler, or set the default payload to an empty object
+    payload = typeof(payload) == 'object'? payload : {};
+
+    // Convert the payload to a string
+    var payloadString = JSON.stringify(payload);
+
+    // Return the response
+    res.setHeader('Content-type','Application/json')
+    res.writeHead(statusCode);
+    res.end(payloadString);
+    console.log(data)
+    console.log("Returning this response: ",statusCode,payloadString);
+
+        });
+
+    });
+}
 
 
-})
+// Define all the handlers
+var handlers = {};
 
-server.listen(3001,()=>{
-    console.log('lestening on port 3001 now')
-})
+// Sample handler
+handlers.sample = function(data,callback){
+    callback(406,{'name':'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data,callback){
+  callback(404);
+};
+
+// Define the request router
+var router = {
+  'sample' : handlers.sample
+};
