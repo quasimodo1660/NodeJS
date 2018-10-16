a6cea1f7cd3ccf2b193dca3c0fc1cdea98aa1785
Quasimodo <quasimodo1660@gmail.com>
2018-10-15 22:38:09 -0700

    section 1

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 //Dependencies
-const http = require('http')
+const http = require('http');
+const url=require('url');
+
+
+
+
 
 const server=http.createServer((req,res)=>{
+    // console.log(req)
+    var parsedUrl=url.parse(req.url,true)
+    // console.log(parsedUrl)
+
+    var path=parsedUrl.pathname
+    var trimmedPath= path.replace(/^\/+|\/+$/g,'')
+
+    console.log(parsedUrl.query)
+
+    console.log(req.method.toLowerCase())
+
+    console.log(trimmedPath)
+    
     res.end('You are SB\n')
+
+
 })
 
 server.listen(3001,()=>{
